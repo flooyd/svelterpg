97638d7a448c7766742f77fe3ce030e457ba0de9
Floyd <floydtjones@gmail.com>
2024-01-29 02:52:49 -0500

    multiple areas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,60 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 
-	let playerX = 0;
-	let playerY = 0;
+	let playerX = 100;
+	let playerY = 100;
 	let lastTime = 0;
 	let area = 0;
 	let playerSpeed = 5;
 	let ready = false;
 	let npcs = [];
-    let nearbyNPC = null;
-    let showNPCMessage = false;
+	let nearbyNPC = null;
+	let showNPCMessage = false;
 
 	const addEnemies = () => {
 		npcs.push({
 			x: 200,
 			y: 300,
-			area: 0
+			area: 0,
+			message: 'Hello, I am an NPC!',
+			background: 'blue',
 		});
 		npcs.push({
 			x: 100,
 			y: 100,
-			area: 1
+			area: 1,
+			message: 'Hello, I am an NPC in area 1!',
+			background: 'blue',
 		});
+		npcs.push({
+			x: 200,
+			y: 100,
+			area: 1,
+			message: 'Hello, I am an NPC in area 1!',
+			background: 'blue',
+		});
+		npcs.push({
+			x: 300,
+			y: 100,
+			area: 3,
+			message: 'Hello, I am an NPC in area 1!',
+			background: 'blue',
+		});
+	};
+
+	const addWall = () => {
+		//add brown walls to left side of area 1
+		for (let i = 0; i < 500; i += 32) {
+			npcs.push({
+				x: 0,
+				y: i,
+				area: 0,
+				background: 'brown',
+				fadeDirection: 'in',
+			});
+		}
+
 	};
 
 	const move = () => {
@@ -75,35 +108,32 @@
 		if (event.key === 'd') {
 			keys.d = false;
 		}
-        if(event.key === 'e') {
-            if(nearbyNPC) {
-                showNPCMessage = true;
-            }
-        }
 	};
 
 	onMount(() => {
 		const gameLoop = () => {
 			move();
+			detectNearbyNPC();
 			requestAnimationFrame(gameLoop);
-            checkForNearbyNPCs();
+			detectCollisions();
 		};
 
 		addEnemies();
+		addWall();
 		gameLoop();
-        
+
 		ready = true;
 	});
 
 	//if player goes offscreen, side scroll
 	const sideScroll = () => {
-		if (playerX > 500) {
+		if (playerX > 500 - 32) {
 			area += 1;
 			playerX = 0;
 		}
 		if (playerX < 0 && area > 0) {
 			area -= 1;
-			playerX = 500;
+			playerX = 500 - 32;
 		}
 		if (playerX < 0 && area === 0) {
 			playerX = 0;
@@ -117,21 +147,46 @@
 		}
 	};
 
-	const checkForNearbyNPCs = () => {
+	const detectNearbyNPC = () => {
+		//detect if player is within 100px of an npc
 		nearbyNPC = npcs.find((npc) => {
-            return (
-                npc.area === area &&
-                npc.x > playerX - 64 &&
-                npc.x < playerX + 64 &&
-                npc.y > playerY - 64 &&
-                npc.y < playerY + 64
-            );
-        });
+			return (
+				npc.area === area &&
+				Math.abs(npc.x - playerX) < 64 &&
+				Math.abs(npc.y - playerY) < 64
+			);
+		}) || null;
 
-        if(!nearbyNPC) {
-            showNPCMessage = false;
-        }
+		if (!nearbyNPC) {
+			showNPCMessage = false;
+		}
+    };
+
+	const detectCollisions = () => {
+		//prevent player from walking through npcs
+		npcs.forEach((npc) => {
+			if (
+				npc.area === area &&
+				Math.abs(npc.x - playerX) < 34 &&
+				Math.abs(npc.y - playerY) < 34
+			) {
+				if (keys.w) {
+					playerY += playerSpeed;
+				}
+				if (keys.a) {
+					playerX += playerSpeed;
+				}
+				if (keys.s) {
+					playerY -= playerSpeed;
+				}
+				if (keys.d) {
+					playerX -= playerSpeed;
+				}
+			}
+		});
 	};
+
+
 </script>
 
 <svelte:window on:keydown={handleKeyDown} on:keyup={handleKeyUp} />
@@ -142,24 +197,33 @@
 		</div>
 		<div class="viewport">
 			<div
-				class="player"
-				style="height: 32px; width: 32px; background-color: black; position: absolute; top: {playerY}px; left: {playerX}px;"
+				class="player character"
+				style="height: 32px; width: 32px; background-color: white; position: absolute; top: {playerY}px; left: {playerX}px;"
 			></div>
 			{#each npcs as npc}
 				{#if npc.area === area}
 					<div
-						class="npc"
-						style="height: 32px; width: 32px; background-color: red; position: absolute; top: {npc.y}px; left: {npc.x}px;"
+						class="npc character"
+						style="height: 32px; width: 32px; background-color: {npc.background}; position: absolute; top: {npc.y}px; left: {npc.x}px;"
 					></div>
-                    {#if nearbyNPC === npc && showNPCMessage}
-                        <div
-                            class="message"
-                            style="position: absolute; top: {npc.y - 32}px; left: {npc.x}px;"
-                        >
-                            Hello friend
-                    </div>
-                    {/if}
+					{#if nearbyNPC === npc && npc.message}
+						<button
+							on:click={() => showNPCMessage = !showNPCMessage}
+							style="position: absolute; top: {npc.y - 32}px; left: {npc.x + 8}px;"
+						>
+							!
+						</button>
+					{/if}
 				{/if}
+				{#if nearbyNPC === npc && showNPCMessage}
+				<div
+					transition:fade={{duration: 50}}
+					class="message"
+					style="position: absolute; bottom: 0px; left: 0px; width: 100%; text-align: center;"
+				>
+					{nearbyNPC.message}
+				</div>
+			{/if}
 			{/each}
 		</div>
 	{/if}
@@ -180,5 +244,16 @@
 		width: 500px;
 		background: white;
 		position: relative;
+		overflow: hidden;
+	}
+
+	.character {
+		border: 1px solid black;
+	}
+
+	.message {
+		border: 1px solid black;
+		background-color: white;
+		height: 100px;
 	}
 </style>
